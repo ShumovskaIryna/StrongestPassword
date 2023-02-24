5fee7195ca177f0a6d594c034abb6517fd9ef091
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-02-24 19:08:02 +0200

    add input for password

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3 className="text-center my-5">Password Form</h3>
+        <div className="form-group mb-1">
+          <input
+            type="password"
+            className="form-control shadow-none"
+            placeholder="Password"
+          />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
