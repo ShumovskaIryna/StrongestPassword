7c64744d1ed27446971b6adcce5e284bfe84cb7b
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-02-25 19:20:13 +0200

    create switch case & use regex

--- src/components/PasswordIndicator.js
+++ src/components/PasswordIndicator.js
@@ -3,6 +3,41 @@ import "./PasswordIndicator.css";
 
 const PasswordIndicator = ( props ) => {
 
+  const pwdValidate = props.password;
+
+  const pwdChecker = () => {
+    let pwdCheck = 0;
+    let validateRegex = ["[A-Z]", "[a-z]", "[0-9]", "\\W"];
+    validateRegex.forEach((regex, i) => {
+      if (new RegExp(regex).test(pwdValidate)) {
+        pwdCheck += 1;
+      }
+    });
+    switch (pwdCheck) {
+      case 0:
+        return {
+          strength: 0,
+          val: "",
+        };
+      case 1:
+        return {
+          strength: 1,
+          val: "easy",
+        };
+      case 2:
+        return {
+          strength: 2,
+          val: "medium",
+        };
+      case 3:
+        return {
+          strength: 3,
+          val: "strong",
+        };
+      default:
+        return null;
+    }
+  };
   return (
     <>
       <div className="wrapper">
@@ -12,7 +47,7 @@ const PasswordIndicator = ( props ) => {
         />
           {props.password && (
               <p className="label">
-              Your password is easy
+              Your password is <strong>{pwdChecker().val} </strong>
               </p>
           )}
       </div>
